9546f9576edf69c0cc8c952019e78309a7784a30
江毅 <903199067@qq.com>
2023-05-06 16:37:59 +0800

    update

--- go.mod
+++ go.mod
@@ -3,14 +3,34 @@ module go-miniblink
 go 1.17
 
 require (
-	github.com/del-xiong/miniblink v0.0.0-20220609045710-8ec016458de6 // indirect
+	github.com/del-xiong/miniblink v0.0.0-20220609045710-8ec016458de6
+	github.com/go-chi/chi v1.5.4
+	github.com/go-vgo/robotgo v0.100.10
+	github.com/joho/godotenv v1.5.1
+)
+
+require (
+	github.com/StackExchange/wmi v1.2.1 // indirect
 	github.com/elazarl/go-bindata-assetfs v1.0.0 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
-	github.com/lxn/win v0.0.0-20191128105842-2da648fda5b4 // indirect
+	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/mushroomsir/mimetypes v0.0.0-20171113080813-3b41529ef757 // indirect
-	golang.org/x/sys v0.0.0-20190904154756-749cb33beabd // indirect
+	github.com/otiai10/gosseract v2.2.1+incompatible // indirect
+	github.com/robotn/gohook v0.31.3 // indirect
+	github.com/robotn/xgb v0.0.0-20190912153532-2cb92d044934 // indirect
+	github.com/robotn/xgbutil v0.0.0-20190912154524-c861d6f87770 // indirect
+	github.com/shirou/gopsutil v3.21.10+incompatible // indirect
+	github.com/tklauser/go-sysconf v0.3.9 // indirect
+	github.com/tklauser/numcpus v0.3.0 // indirect
+	github.com/vcaesar/gops v0.21.3 // indirect
+	github.com/vcaesar/imgo v0.30.0 // indirect
+	github.com/vcaesar/keycode v0.10.0 // indirect
+	github.com/vcaesar/tt v0.20.0 // indirect
+	golang.org/x/image v0.0.0-20211028202545-6944b10bf410 // indirect
+	golang.org/x/sys v0.0.0-20211123173158-ef496fb156ab // indirect
 )
 
-replace github.com/del-xiong/miniblink v0.0.0-20220609045710-8ec016458de6 => github.com/jiangyimm/miniblink v0.0.0-20230505060905-8f8457c76225
+replace github.com/del-xiong/miniblink v0.0.0-20220609045710-8ec016458de6 => github.com/jiangyimm/miniblink v0.0.0-20230505084835-f67d045a50be
--- main.go
+++ main.go
@@ -1,38 +1,156 @@
 package main
 
+//go build  -tags bdebug -ldflags "-H windowsgui" -o SynyiQcLocalproxy.exe  打包成后台应用，带devtools
+//双击exe启动
+//http://localhost:3000 查看是否运行
+//http://localhost:3000/inpat?inpatId=ZY230417002&emplCode=99999&hospCode=0101 打开/刷新/切换患者
+//http://localhost:3000/close 关闭质控医生端
+
 import (
-	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/del-xiong/miniblink"
+	"github.com/go-chi/chi"
+	"github.com/go-vgo/robotgo"
+	"github.com/joho/godotenv"
+)
+
+var (
+	view *miniblink.WebView
+
+	is_debug           bool
+	qc_win_title       string
+	qc_address         string
+	qc_localproxy_port string
+	qc_width           int
+	qc_height          int
+	qc_top             int
+	qc_left            int
+
+	emplCode string
+	hospCode string
 )
 
 func main() {
+	//读取配置
+	readConfig()
+
+	//初始化 blink
+	initBlink()
+
+	//启动server
+	startServe()
+}
+
+func readConfig() {
+	// 获取屏幕大小
+	width, _ := robotgo.GetScreenSize()
+
+	if e := godotenv.Load(); e != nil {
+		log.Fatal(e)
+	}
+
+	is_debug, _ = strconv.ParseBool(os.Getenv("is_debug"))
+	qc_win_title = os.Getenv("qc_win_title")
+
+	qc_address = os.Getenv("qc_address")
+	qc_localproxy_port = os.Getenv("qc_localproxy_port")
+
+	qc_width, _ = strconv.Atoi(os.Getenv("qc_width"))
+	qc_height, _ = strconv.Atoi(os.Getenv("qc_height"))
+	qc_top, _ = strconv.Atoi(os.Getenv("qc_top"))
+	var right, _ = strconv.Atoi(os.Getenv("qc_right"))
+	qc_left = width - right - qc_width
+}
+
+func initBlink() {
 	//设置调试模式
-	miniblink.SetDebugMode(true)
+	miniblink.SetDebugMode(is_debug)
 	//初始化miniblink模块
 	err := miniblink.InitBlink()
 	if err != nil {
 		log.Fatal(err)
 	}
-	// 启动1366x920普通浏览器
-	view := miniblink.NewWebView(false, 350, 850, 1500, 10)
-	// 启动1366x920透明浏览器(只有web界面会显示)
-	//view := miniblink.NewWebView(true, 1366, 920)
-	view.LoadURL("http://10.1.72.55:31090/third/control")
-	orgCode := "0101"
-	emplCode := "99999"
-	var user string = `{"orgCode":"` + orgCode + `","emplCode":"` + emplCode + `"}`
-	var setUserJS string = `localStorage.setItem('user','` + user + `')`
-	fmt.Println(user)
-	view.Eval(setUserJS)
-	view.LoadURL("http://10.1.72.55:31090/third/control?inpatId=ZY230427002")
-	// 设置窗体标题(会被web页面标题覆盖)
-	view.SetWindowTitle("森亿电子病历内涵质控系统")
-	view.DisableAutoTitle()
+}
+
+func startServe() {
+	r := chi.NewRouter()
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("this is local proxy serve for synyi quality control system"))
+	})
+	r.Get("/inpat", inpat)
+	r.Get("/close", closeView)
+	http.ListenAndServe(":"+qc_localproxy_port, r)
+}
+
+func inpat(rw http.ResponseWriter, r *http.Request) {
+	inpatId := r.URL.Query().Get("inpatId")
+	url_emplCode := r.URL.Query().Get("emplCode")
+	url_hospCode := r.URL.Query().Get("hospCode")
+	isSetUser := (len(emplCode) > 0 && len(hospCode) > 0) && (url_emplCode != emplCode || url_hospCode != hospCode)
+	emplCode = url_emplCode
+	hospCode = url_hospCode
+
+	if view != nil {
+		if view.IsDestroy {
+			go newView(inpatId)
+		} else {
+			go refreshView(isSetUser, inpatId)
+		}
+	} else {
+		go newView(inpatId)
+	}
+}
+
+func closeView(rw http.ResponseWriter, r *http.Request) {
+	if view != nil {
+		view.DestroyWindow()
+	}
+}
+
+func newView(inpatId string) {
+	// 启动浏览器
+	view = miniblink.NewWebView(false, qc_width, qc_height, qc_left, qc_top)
+	// 启动浏览器(只有web界面会显示)
+	//view := miniblink.NewWebView(false, qc_width, qc_height, qc_left, qc_top)
+	view.LoadURL(qc_address)
+
+	setUser()
+
+	view.LoadURL(qc_address + "?inpatId=" + inpatId)
 
 	// 显示窗口
 	view.ShowWindow()
 
-	<-make(chan bool)
+	//debug
+	view.HideDockIcon()
+	setWinTitle()
+	view.MostTop(true)
+}
+
+func refreshView(isSetUser bool, inpatId string) {
+	if isSetUser {
+		setUser()
+	}
+
+	view.LoadURL(qc_address + "?inpatId=" + inpatId)
+
+	setWinTitle()
+
+	view.RestoreWindow()
+}
+
+func setUser() {
+	var user string = `{"orgCode":"` + hospCode + `","emplCode":"` + emplCode + `"}`
+	var setUserJS string = `localStorage.setItem('user','` + user + `')`
+	view.Eval(setUserJS)
+}
+
+func setWinTitle() {
+	// 设置窗体标题(会被web页面标题覆盖)
+	view.SetWindowTitle(qc_win_title)
+	view.DisableAutoTitle()
 }
